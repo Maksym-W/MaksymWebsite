34155277bfe2b7a766660156100e72801a5f1a5b
Maksym-W <maksymwoychyshyn@gmail.com>
2024-06-14 16:02:32 -0400

    Updates

---
+++ precache-manifest.90cc6c23e93fba55bc80fd2391373ec0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385f48219277ea2e7d9979dad81ed899",
+    "url": "/src/index.html"
+  },
+  {
+    "revision": "9f426985a735a218dcb7",
+    "url": "/src/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e294af0f0f022f3cdb1f",
+    "url": "/src/static/js/2.a2e48fdf.chunk.js"
+  },
+  {
+    "revision": "5ecf7126cbb577f0b93fb1815eaef796",
+    "url": "/src/static/js/2.a2e48fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6e8b180d66e76a42b9",
+    "url": "/src/static/js/3.8b130bed.chunk.js"
+  },
+  {
+    "revision": "9f426985a735a218dcb7",
+    "url": "/src/static/js/main.d384df52.chunk.js"
+  },
+  {
+    "revision": "d8a26733477ad4c76624",
+    "url": "/src/static/js/runtime-main.1e09b411.js"
+  },
+  {
+    "revision": "489d844c21a193b9d606a202ed314404",
+    "url": "/src/static/media/Robotics1.489d844c.jpg"
+  },
+  {
+    "revision": "067dee569a2022d736b999be6113cfbc",
+    "url": "/src/static/media/max.067dee56.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/src/index.js/precache-manifest.cad6498a7e19b20c5816d5d536c0fadb.js"
+  "/src/precache-manifest.90cc6c23e93fba55bc80fd2391373ec0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/src/index.js/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/src/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
