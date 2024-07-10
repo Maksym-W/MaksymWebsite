a1ec425a8a3ed069fd72f9991f2e506398cf6395
Maksym-W <maksymwoychyshyn@gmail.com>
2024-07-10 19:52:25 -0400

    Updates

---
+++ precache-manifest.59fef3a4462d4cd379404a0466bd37da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4448a33605fea696f866390e7cab9245",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26cc98d025cc1fcb7c6a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e294af0f0f022f3cdb1f",
+    "url": "/static/js/2.a2e48fdf.chunk.js"
+  },
+  {
+    "revision": "5ecf7126cbb577f0b93fb1815eaef796",
+    "url": "/static/js/2.a2e48fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6e8b180d66e76a42b9",
+    "url": "/static/js/3.8b130bed.chunk.js"
+  },
+  {
+    "revision": "26cc98d025cc1fcb7c6a",
+    "url": "/static/js/main.7f30a395.chunk.js"
+  },
+  {
+    "revision": "dbe9a9a147258213a3fe",
+    "url": "/static/js/runtime-main.ae9843fe.js"
+  },
+  {
+    "revision": "489d844c21a193b9d606a202ed314404",
+    "url": "/static/media/Robotics1.489d844c.jpg"
+  },
+  {
+    "revision": "067dee569a2022d736b999be6113cfbc",
+    "url": "/static/media/max.067dee56.jpg"
+  }
+]);
