d2271e0f9b1335f9ca58971328fd573ccd61d50b
Maksym-W <maksymwoychyshyn@gmail.com>
2024-09-22 11:51:49 -0400

    Changed the page a bit

--- src/pages/CsProjects.js
+++ src/pages/CsProjects.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Robotics1 from '../images/Robotics1.jpg';
 import AnimatedPage from '../animations/transitions';
 import { Button } from '@mui/material';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+
 
 export default function CsProjects() {
     const pictureStyle = {
@@ -26,6 +32,82 @@ export default function CsProjects() {
         fontSize: '16px',
       };
 
+    const accordionStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        border: '1px solid #e4ddcb',
+    };
+
+    const codeString = `/**
+    Maksym's Drink Dispencer Machine: Rizz Spencer
+ 
+    Copyright (C) 2024
+    I have no idea how licenses work. 
+ */
+ 
+ #include <LiquidCrystal.h>
+ #include <Keypad.h>
+ 
+ /* Handles the pump setup */
+ #define PUMP_1 8
+ #define PUMP_2 9
+ #define PUMP_3 10
+ #define PUMP_4 11
+ #define PUMP_5 12
+ #define PUMP_6 13
+ const int pumpPins[] = {PUMP_1, PUMP_2, PUMP_3, PUMP_4, PUMP_5, PUMP_6};
+ 
+ /* Handles the Display setup */
+ LiquidCrystal lcd(A5, A4, A3, A2, A1, A0);
+ 
+ /* Handles the Keypad setup */
+ const byte KEYPAD_ROWS = 4;
+ const byte KEYPAD_COLS = 4;
+ byte rowPins[KEYPAD_ROWS] = {7, 6, 5, 4};
+ byte colPins[KEYPAD_COLS] = {3, 2, 1, 0};
+ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
+   {'1', '2', '3', 'A'},
+   {'4', '5', '6', 'B'},
+   {'7', '8', '9', 'C'},
+   {'*', '0', '#', 'D'}
+ };
+ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+ 
+ void setup() {
+   lcd.begin(16, 2);
+   lcd.print("The Rizz-Spencer");
+ 
+   for (int i = 0; i < 6; i++) {
+     pinMode(pumpPins[i], OUTPUT);
+   }
+ }
+ 
+ void activatePump(int pumpNumber) {
+   for (int i = 0; i < 6; i++) {
+     digitalWrite(pumpPins[i], LOW);
+   }
+ 
+   if (pumpNumber >= 0 && pumpNumber < 6) {
+     digitalWrite(pumpPins[pumpNumber] - 1, HIGH);
+   }
+   lcd.print("Making Drink " + String(pumpNumber));
+   delay(1000); // Change this if you want Spencer to Dispence for more then 1 second.
+ }
+ 
+ void loop() {
+   lcd.setCursor(0, 2);
+   char customKey = keypad.getKey();
+ 
+   if (customKey){ 
+     activatePump((int(customKey) - 48)); // Don't forget to shift by 48 to get integers that aren't gibberish!
+   } else {
+     for (int i = 0; i < 6; i++) {
+       digitalWrite(pumpPins[i], LOW);
+     }
+     lcd.print("Making Nothing   ");
+   }
+ }`; // TODO Put this into another file, and import it so 
+
 // Above is CSS code
 
     return (
@@ -34,8 +116,22 @@ export default function CsProjects() {
                 <h3 style={{ color: 'white', flex: '0 0 45%', marginLeft: '30px' }}>
                 This is the main project that I have been working on as of late. It functions like a keurig machine,
                  only that it has tubes sticking out of it that you can put into a bottle of coca-cola/pepsi/etc and it can make a drink for you. 
-                In the images below, you can see the current state of it. 
-                I still need to code out the arduino board and connect the pumps to it. Il upload the code when I have time to make it 
+                In the images below, you can see the current state of it. I will wire the thing up soon, and upload pictures of it. 
+
+                <Accordion style={accordionStyle}>
+                    <AccordionSummary 
+                        style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                    >
+                        <Typography>Click me to see the code for the robot.</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <pre style={{ whiteSpace: 'pre-wrap', color: 'white' }}>
+                            <code>{codeString}</code>
+                        </pre>
+                    </AccordionDetails>
+                </Accordion>
+
+
                 <hr></hr>
                 Im also working on a video game adaptation of blood on the clocktower. Currently, the code just sets up a server that other 
                 computers on the local network can connect to, plus some incomplete game logic. Will upload more details about it soon.
