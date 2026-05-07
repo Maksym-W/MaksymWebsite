5c2f63032910aee1a199433ff907de0ad621fee4
Maksym-W <maksymwoychyshyn@gmail.com>
2026-05-07 14:53:26 -0400

    Putting everything in accordions

--- src/pages/CsProjects.js
+++ src/pages/CsProjects.js
@@ -10,9 +10,10 @@ import braille from '../images/braille.jpg';
 
 export default function CsProjects() {
     const accordionStyle = {
-        backgroundColor: 'black',
+        backgroundColor: '#282c34',
         color: 'white',
         border: '1px solid #e4ddcb',
+        margin: 0,
     };
 
     const pictureStyle = {
@@ -102,32 +103,61 @@ export default function CsProjects() {
     return (
         <AnimatedPage>
                 <div style={{ display: 'flex', flexWrap: 'flex-start', marginTop: '20px' }}>
-                    <h3 style={{ color: 'white', flex: '0 0 45%', marginLeft: '30px' }}>
-                    The main project that I will be working on this summer is a braille device reader.
-                    I originally started this project with&nbsp;
-                    <a href="https://www.linkedin.com/in/jessica-wen1021/" target =" blank" style={{color: 'cyan', 'text-align': 'center' }}>Jessica Wen</a> and&nbsp;
-                    <a href="https://www.linkedin.com/in/devin-mendes-24b609330/" target =" blank" style={{color: 'cyan', 'text-align': 'center' }}>Devin Mendes</a>&nbsp;
-                    in collaboration with Dr. Daniel Zingaro at UTM, and we created a solenoid based braille reader,
-                    where the solenoids are controlled with relays and an Arduino. We aim to continue working on this
-                    during the summer to improve its capabilities, while ensuring it keeps a low cost per braille cell (less then 20 dollars per cell). 
-                    <hr></hr>
+                    <h3 style={{ color: 'white', marginLeft: '30px', marginRight: '30px' }}>
 
                     This is the main project that I have been working on as of late. It functions like a keurig machine,
                     only that it has tubes sticking out of it that you can put into a bottle of coca-cola/pepsi/etc and it can make a drink for you. 
                     The video right next to me. I will wire the thing up soon, and upload pictures of it. 
 
-                    <Accordion style={accordionStyle}>
-                        <AccordionSummary 
-                            style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-                        >
-                            <Typography>Click me to see the code for the robot.</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <pre style={{ whiteSpace: 'pre-wrap', color: 'white' }}>
-                                <code>{codeString}</code>
-                            </pre>
-                        </AccordionDetails>
-                    </Accordion>
+                    <div>
+                      <Accordion style={accordionStyle}>
+                          <AccordionSummary style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                              <Typography>Drink Robot</Typography>
+                          </AccordionSummary>
+                          <AccordionDetails>
+                              The main project that I will be working on this summer is a braille device reader.
+                              I originally started this project with&nbsp;
+                              <a href="https://www.linkedin.com/in/jessica-wen1021/" target =" blank" style={{color: 'cyan', 'text-align': 'center' }}>Jessica Wen</a> and&nbsp;
+                              <a href="https://www.linkedin.com/in/devin-mendes-24b609330/" target =" blank" style={{color: 'cyan', 'text-align': 'center' }}>Devin Mendes</a>&nbsp;
+                              in collaboration with Dr. Daniel Zingaro at UTM, and we created a solenoid based braille reader,
+                              where the solenoids are controlled with relays and an Arduino. We aim to continue working on this
+                              during the summer to improve its capabilities, while ensuring it keeps a low cost per braille cell (less then 20 dollars per cell). 
+
+                              <pre style={{ whiteSpace: 'pre-wrap', color: 'white' }}>
+                                <iframe
+                                  width="560"
+                                  height="315"
+                                  src="https://www.youtube.com/embed/36Ka0xX_gws"
+                                  title="YouTube video player"
+                                  frameBorder="0"
+                                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                  allowFullScreen
+                                  style={{
+                                    flex: '0 0 30%',
+                                    marginLeft: '60px',
+                                    marginTop: '50px',
+                                    border: '25px solid #e4ddca',
+                                    borderImage: 'linear-gradient(to bottom, #e4ddca, transparent) 1 49%',
+                                  }}/>
+                                  <code>{codeString}</code>
+                              </pre>
+                          </AccordionDetails>
+                      </Accordion>
+                      <Accordion style={accordionStyle}>
+                          <AccordionSummary style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                              <Typography>Braille Device</Typography>
+                          </AccordionSummary>
+                          <AccordionDetails>
+                              This is the main project that I have been working on as of late. It functions like a keurig machine,
+                              only that it has tubes sticking out of it that you can put into a bottle of coca-cola/pepsi/etc and it can make a drink for you. 
+                              The video right next to me. I will wire the thing up soon, and upload pictures of it. 
+                              <pre style={{ whiteSpace: 'pre-wrap', color: 'white' }}>
+                                  <img src={braille} alt="Drink Machine" style={{ flex: '0 0 30%', maxWidth: '500px', marginLeft: '60px', ...pictureStyle }}/>
+                              </pre>
+                          </AccordionDetails>
+                      </Accordion>  
+                    </div>
+
 
                     <hr></hr>
                     I have also worked on plenty of school projects. I have built a game engine in the computer graphics courses I took.
@@ -139,31 +169,6 @@ export default function CsProjects() {
                     </a>
                     </h3>
 
-                <div style={{flex: '0 0 200px', display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '80px',}}>
-                
-                {/* <a href="#flip" target="_blank" rel="noopener noreferrer" >
-                <Button variant="contained" style={navItemStyle}>Flip</Button>
-                </a> */}
-
-                {<img src={braille} alt="Drink Machine" style={{ flex: '0 0 30%', maxWidth: '500px', marginLeft: '60px', ...pictureStyle }}/>} 
-
-                <iframe
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/36Ka0xX_gws"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  style={{
-                    flex: '0 0 30%',
-                    marginLeft: '60px',
-                    marginTop: '50px',
-                    border: '25px solid #e4ddca',
-                    borderImage: 'linear-gradient(to bottom, #e4ddca, transparent) 1 49%',
-                  }}
-                />
-                </div>
             </div>
         </AnimatedPage>
     )
